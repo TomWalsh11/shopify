37c71394eb6c0a21cdc1a648ad1d342b9b6344b8
TomWalsh11 <105279757+TomWalsh11@users.noreply.github.com>
2022-06-07 16:26:18 +0100

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,11 +1,18 @@
+Q1
 SELECT COUNT(*) FROM Orders
 LEFT JOIN Shippers ON Shippers.ShipperID = Orders.ShipperID
 WHERE Shippers.ShipperName = 'Speedy Express';
 
+54
+
+Q2
 SELECT LastName FROM Employees
 WHERE EmployeeID = (SELECT TOP 1 EmployeeID FROM
 (SELECT EmployeeID, COUNT(OrderID) FROM Orders GROUP BY EmployeeID ORDER BY COUNT(OrderID) DESC));
 
+Peacock
+
+Q3
 SELECT ProductName, SUM(Quantity) AS Qty FROM
 (((Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID)
 INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID)
@@ -13,3 +20,5 @@ INNER JOIN Products ON OrderDetails.ProductID = Products.ProductID)
 WHERE Country = 'Germany'
 GROUP BY ProductName
 ORDER BY SUM(Quantity) DESC
+
+Boston Crab Meat - 160
